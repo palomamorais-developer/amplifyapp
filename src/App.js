af26884d650b937d085097377d17ba143b928f38
Vini <palomamorais.developer@gmail.com>
2022-09-18 21:38:39 -0300

    Authentication with Auth0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import "./App.css";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 function App() {
+  const { isLoading } = useAuth0();
+  if (isLoading) return <div>isLoading...</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </>
   );
 }
 
